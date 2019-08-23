4965eeb0e09877e5ca88e253c0b89b01264f6167
klamparski <klamparski@gmail.com>
2019-08-23 18:09:40 +0200

    #569 Adds default serialization for news FileReference model to avoid errors when using context without serialization groups

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,5 +9,12 @@ call_user_func(
 
         ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3apinews/Configuration/TsConfig/Page/mod.tsconfig">');
 
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['t3api']['serializerMetadataDirs'] = array_merge(
+            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['t3api']['serializerMetadataDirs'] ?? [],
+            [
+                'GeorgRinger\News' => ExtensionManagementUtility::extPath('t3apinews') . 'Resources/Private/Serializer/GeorgRinger.News',
+            ]
+        );
+
     }
 );
