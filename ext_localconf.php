cdd7261bcb814fc7301ff101e6d7bc8f6b89c196
Krystian Linski <krystian.linski@gmail.com>
2019-08-30 12:55:26 +0200

    Remove use from ext_localconf.php and ext_tables.php files

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,16 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
     function () {
 
-        ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3apinews/Configuration/TsConfig/Page/mod.tsconfig">');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3apinews/Configuration/TsConfig/Page/mod.tsconfig">');
 
         $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['t3api']['serializerMetadataDirs'] = array_merge(
             $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['t3api']['serializerMetadataDirs'] ?? [],
             [
-                'GeorgRinger\News' => ExtensionManagementUtility::extPath('t3apinews') . 'Resources/Private/Serializer/GeorgRinger.News',
+                'GeorgRinger\News' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('t3apinews') . 'Resources/Private/Serializer/GeorgRinger.News',
             ]
         );
 
--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,5 @@
 <?php
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3_MODE') || die('Access denied.');
 
-ExtensionManagementUtility::addStaticFile('t3apinews', 'Configuration/TypoScript', 'T3api sample for news ext');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3apinews', 'Configuration/TypoScript', 'T3api sample for news ext');
