30da197974a30d5725d22004917e1821bcfe76ee
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2022-06-10 10:58:40 +0200

    Update dependencies. Set new stability level.

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,12 +6,12 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'author' => '',
     'author_email' => '',
-    'state' => 'beta',
-    'version' => '0.3.0',
+    'state' => 'stable',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.999',
-            'news' => '7.0.0-8.99.999',
+            'news' => '8.0.0-9.99.999',
             't3api' => '1.0.0-2.99.999',
         ],
         'conflicts' => [],
